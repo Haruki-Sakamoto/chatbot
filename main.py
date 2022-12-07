7882294842b55f175d8f7aa0d27b07e8d01be723
Haruki Sakamoto <harukix8686@gmail.com>
2022-12-07 21:24:48 +0900

    ngrokで機能確認出来る様にした

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     # ジョブを実行する時間を登録する。
     schedule.every().day.at('06:00').do(notice.gomi_check_and_message)
     schedule.every().day.at('23:00').do(notice.bed_time_notice)
-    schedule.every().day.at('20:38').do(notice.bed_time_notice)
+    schedule.every().day.at('21:00').do(notice.bed_time_notice)
 
     # 60秒おきにジョブの実行条件を満たしているか確認する。
     while True:
