afdd75443d0569a59e59813c6aee66b07ab0e925
Haruki Sakamoto <harukix8686@gmail.com>
2022-12-12 21:57:40 +0900

    scheduleジョブの追加

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from flask import Flask
+import schedule
+import time
+
+import notice
 
 app = Flask(__name__)
 
 @app.route('/')
 def is_active():
     return 'line-bot起動中' 
+
+if __name__ == '__main__':
+    # ジョブを実行する時間を登録する。
+    schedule.every().day.at('06:00').do(notice.gomi_check_and_message)
+    schedule.every().day.at('23:00').do(notice.bed_time_notice)
+    schedule.every().day.at('21:00').do(notice.bed_time_notice)
+        
+    # 60秒おきにジョブの実行条件を満たしているか確認する。
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
