651d80096af4055ef05a5d1e9295001f260e5318
Haruki Sakamoto <harukix8686@gmail.com>
2022-12-12 22:39:31 +0900

    アクセス時にスケジュール機動

--- main.py
+++ main.py
@@ -8,18 +8,29 @@ app = Flask(__name__)
 
 @app.route('/')
 def is_active():
-    return 'line-bot起動中' 
-
-if __name__ == 'main':
     # ジョブを実行する時間を登録する。
     schedule.every().day.at('06:00').do(notice.gomi_check_and_message)
     schedule.every().day.at('23:00').do(notice.bed_time_notice)
     schedule.every().day.at('21:00').do(notice.bed_time_notice)
-    schedule.every().day.at('22:35').do(notice.bed_time_notice)
-    schedule.every().day.at('22:40').do(notice.bed_time_notice)
+    schedule.every().day.at('22:45').do(notice.bed_time_notice)
     schedule.every().day.at('22:50').do(notice.bed_time_notice)
+    schedule.every().day.at('22:55').do(notice.bed_time_notice)
         
     # 60秒おきにジョブの実行条件を満たしているか確認する。
     while True:
         schedule.run_pending()
         time.sleep(1)
+
+# if __name__ == 'main':
+#     # ジョブを実行する時間を登録する。
+#     schedule.every().day.at('06:00').do(notice.gomi_check_and_message)
+#     schedule.every().day.at('23:00').do(notice.bed_time_notice)
+#     schedule.every().day.at('21:00').do(notice.bed_time_notice)
+#     schedule.every().day.at('22:45').do(notice.bed_time_notice)
+#     schedule.every().day.at('22:50').do(notice.bed_time_notice)
+#     schedule.every().day.at('22:55').do(notice.bed_time_notice)
+        
+#     # 60秒おきにジョブの実行条件を満たしているか確認する。
+#     while True:
+#         schedule.run_pending()
+#         time.sleep(1)
