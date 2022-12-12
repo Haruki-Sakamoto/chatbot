06c4d854f9833383e8fb9208455ff914af568321
Haruki Sakamoto <harukix8686@gmail.com>
2022-12-12 20:47:44 +0900

    1分置きのスケジュールを追加した

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ import notice
 
 if __name__ == '__main__':
     # ジョブを実行する時間を登録する。
+    schedule.every(1).minutes.do(notice.bed_time_notice)
     schedule.every().day.at('06:00').do(notice.gomi_check_and_message)
     schedule.every().day.at('23:00').do(notice.bed_time_notice)
     schedule.every().day.at('21:00').do(notice.bed_time_notice)
-
+        
     # 60秒おきにジョブの実行条件を満たしているか確認する。
     while True:
         schedule.run_pending()
