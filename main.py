77466e5563440d3025f50eb74f6f44d42b99d846
Haruki Sakamoto <harukix8686@gmail.com>
2022-12-12 21:13:22 +0900

    flaskでルーティングを設定した

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+from flask import Flask
 import schedule
 import time
 
 import notice
 
+app = Flask(__name__)
+
+@app.route('/')
+def is_active():
+    return 'line-bot起動中' 
+
 if __name__ == '__main__':
+    app.run()
     # ジョブを実行する時間を登録する。
-    schedule.every(1).minutes.do(notice.bed_time_notice)
     schedule.every().day.at('06:00').do(notice.gomi_check_and_message)
     schedule.every().day.at('23:00').do(notice.bed_time_notice)
     schedule.every().day.at('21:00').do(notice.bed_time_notice)
