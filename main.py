c126b20b7fa0c1864262a0e0ec62d527b3aff442
Haruki Sakamoto <harukix8686@gmail.com>
2022-12-12 22:59:27 +0900

    外部からcronで叩くように修正

--- main.py
+++ main.py
@@ -4,24 +4,22 @@ import time
 
 import notice
 
+# ジョブを実行する時間を登録する。
+schedule.every().day.at('06:00').do(notice.gomi_check_and_message)
+schedule.every().day.at('23:00').do(notice.bed_time_notice)
+schedule.every().day.at('21:00').do(notice.bed_time_notice)
+schedule.every().day.at('23:00').do(notice.bed_time_notice)
+schedule.every().day.at('23:05').do(notice.bed_time_notice)
+schedule.every().day.at('23:10').do(notice.bed_time_notice)
+
 app = Flask(__name__)
 
 @app.route('/')
 def is_active():
-    # ジョブを実行する時間を登録する。
-    schedule.every().day.at('06:00').do(notice.gomi_check_and_message)
-    schedule.every().day.at('23:00').do(notice.bed_time_notice)
-    schedule.every().day.at('21:00').do(notice.bed_time_notice)
-    schedule.every().day.at('22:45').do(notice.bed_time_notice)
-    schedule.every().day.at('22:50').do(notice.bed_time_notice)
-    schedule.every().day.at('22:55').do(notice.bed_time_notice)
-        
-    # 60秒おきにジョブの実行条件を満たしているか確認する。
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
-
-# if __name__ == 'main':
+    schedule.run_pending()
+    return 'line-bot起動中'
+    
+# if __name__ == '__main__':
 #     # ジョブを実行する時間を登録する。
 #     schedule.every().day.at('06:00').do(notice.gomi_check_and_message)
 #     schedule.every().day.at('23:00').do(notice.bed_time_notice)
