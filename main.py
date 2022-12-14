b8a46c3a5bc54c5dfef6025a92a50d0fc8e7dd06
Haruki Sakamoto <harukix8686@gmail.com>
2022-12-14 20:35:57 +0900

    main.pyにあるscheduleの位置を変更した

--- main.py
+++ main.py
@@ -8,26 +8,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def is_active():
+    schedule.run_pending()
+    return 'line-bot起動中'
+    
+if __name__ == '__main__':
     # ジョブを実行する時間を登録する。
     schedule.every().day.at('06:00').do(notice.gomi_check_and_message)
     schedule.every().day.at('23:00').do(notice.bed_time_notice)
     schedule.every().day.at('21:00').do(notice.bed_time_notice)
-    schedule.every().day.at('23:15').do(notice.bed_time_notice)
-    schedule.every().day.at('23:20').do(notice.bed_time_notice)
-    schedule.every().day.at('23:25').do(notice.bed_time_notice)
-    schedule.run_pending()
-    return 'line-bot起動中'
-    
-# if __name__ == '__main__':
-#     # ジョブを実行する時間を登録する。
-#     schedule.every().day.at('06:00').do(notice.gomi_check_and_message)
-#     schedule.every().day.at('23:00').do(notice.bed_time_notice)
-#     schedule.every().day.at('21:00').do(notice.bed_time_notice)
-#     schedule.every().day.at('22:45').do(notice.bed_time_notice)
-#     schedule.every().day.at('22:50').do(notice.bed_time_notice)
-#     schedule.every().day.at('22:55').do(notice.bed_time_notice)
+    schedule.every().day.at('22:45').do(notice.bed_time_notice)
+    schedule.every().day.at('22:50').do(notice.bed_time_notice)
+    schedule.every().day.at('22:55').do(notice.bed_time_notice)
+    schedule.every(1).minutes.do(notice.bed_time_notice)
         
-#     # 60秒おきにジョブの実行条件を満たしているか確認する。
-#     while True:
-#         schedule.run_pending()
-#         time.sleep(1)
+    # # 60秒おきにジョブの実行条件を満たしているか確認する。
+    # while True:
+    #     schedule.run_pending()
+    #     time.sleep(1)
