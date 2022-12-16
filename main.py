6eb44b04b76c3336b27f360d848168bf3869b8c5
Haruki Sakamoto <harukix8686@gmail.com>
2022-12-16 22:32:38 +0900

    1分おきにメッセージを飛ばすよおきにメッセージを飛ばすように設定メッセージを飛ばすように設定

--- main.py
+++ main.py
@@ -4,23 +4,29 @@ import time
 
 import notice
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/')
-def is_active():
-    schedule.run_pending()
-    return 'line-bot起動中'
+# @app.route('/')
+# def is_active():
+#     schedule.run_pending()
+#     return 'line-bot起動中'
     
 
-schedule.every().day.at('06:00').do(notice.gomi_check_and_message)
-schedule.every().day.at('23:00').do(notice.bed_time_notice)
-schedule.every().day.at('20:52').do(notice.bed_time_notice)
-schedule.every().day.at('20:55').do(notice.bed_time_notice)
-schedule.every().day.at('20:58').do(notice.bed_time_notice)
-schedule.every().day.at('21:00').do(notice.bed_time_notice)
-schedule.every(1).minutes.do(notice.bed_time_notice)
-        
+# schedule.every().day.at('06:00').do(notice.gomi_check_and_message)
+# schedule.every().day.at('06:00').do(notice.gomi_check_and_message)
+# schedule.every().day.at('06:00').do(notice.gomi_check_and_message)
+# schedule.every().day.at('06:00').do(notice.gomi_check_and_message)
+# schedule.every().day.at('23:00').do(notice.bed_time_notice)
+
+if __name__ == '__main__':
+    schedule.every().day.at('06:00').do(notice.gomi_check_and_message)
+    schedule.every().day.at('07:00').do(notice.gomi_check_and_message)
+    schedule.every().day.at('08:00').do(notice.gomi_check_and_message)
+    schedule.every().day.at('09:00').do(notice.gomi_check_and_message)
+    schedule.every().day.at('23:00').do(notice.bed_time_notice)
+    schedule.every(2).minutes.do(notice.bed_time_notice)
     # # 60秒おきにジョブの実行条件を満たしているか確認する。
-    # while True:
-    #     schedule.run_pending()
-    #     time.sleep(1)
+    while True:
+        schedule.run_pending()
+        # time.sleep(60 * 15)
+        time.sleep(60)
